3ed12802dbe868bcc8bcb4352af9bde946725776
Vojagantoj <kyznecev27@gmail.com>
2025-11-10 16:45:45 +0300

    add model user

---
+++ src/dao/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+
+
+class User(Base):
+    __tablename__ = 'newbot_user'
+
+    telegram_id = Column(Integer, primary_key=True) # ID ползователя Telegram
+    username = Column(String(50), nullable=True) # @username (может быть None, если скрыт)
+    first_name = Column(String(50), nullable=False) # Имя
+    last_name = Column(String(50), nullable=True) # Фамилия(может быть None)
+    phone = Column(String(20), nullable=True)  # Телефон (если пользователь его предоставит)
+    is_admin = Column(Boolean, default=False) # Админ ли?
+    is_active = Column(Boolean, default=True) # Активен ли аккаунт?
+    registered_at = Column(DateTime, default=datetime.utcnow) # Дата регистрации
+
+def __repr__(self):
+    return f'<User:(id={self.telegram_id}, username='{self.username}')>'
+
