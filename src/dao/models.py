fc8a730bc5105f5af45f85275433e0126ee8bf15
Vojagantoj <kyznecev27@gmail.com>
2025-11-10 16:46:39 +0300

    add model user

--- src/dao/models.py
+++ src/dao/models.py
@@ -16,7 +16,7 @@ class User(Base):
     phone = Column(String(20), nullable=True)  # Телефон (если пользователь его предоставит)
     is_admin = Column(Boolean, default=False) # Админ ли?
     is_active = Column(Boolean, default=True) # Активен ли аккаунт?
-    registered_at = Column(DateTime, default=datetime.utcnow) # Дата регистрации
+    registered_at = Column(DateTime, default=datetime.now(tz=UTC)) # Дата регистрации
 
 def __repr__(self):
     return f'<User:(id={self.telegram_id}, username='{self.username}')>'
